0cba59ebd0b6c06cf6258f4ed018430b5c94b570
Gilvan Henrique <gilvanh.15@gmail.com>
2020-07-27 12:06:44 -0300

    import components carousel banner and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import dadosIniciais from './data/dados_iniciais.json';
 import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div>
+    <div style={{ background: "#141414" }}>
       <Menu />
-      <h1>HelloWorld!</h1>
+      
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é front-end? Trabalhando na área."}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[3]}
+      /> 
+      
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[5]}
+      />
+
+      <Footer />
+
     </div>
   ); 
 }
