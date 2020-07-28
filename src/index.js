835968d1e5e1685ce13a65d50aeeaf5346dd307a
Gilvan Henrique <gilvanh.15@gmail.com>
2020-07-28 19:36:47 -0300

    add react-router-dom and testing some route

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function CadastroVideo() {
+  return (
+    <div>
+      Página de Cadastro de Video
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo}/>
+      <Route path="/" component={App}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
